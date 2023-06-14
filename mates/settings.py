29d3c36d83affa97f6a0f7f3b439cf2b1785f9d3
maximo-cobeaga <maximocobeaga@gmail.com>
2023-06-14 20:07:02 -0300

    Un commit

--- mates/settings.py
+++ mates/settings.py
@@ -140,7 +140,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
